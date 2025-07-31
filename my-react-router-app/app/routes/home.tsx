5f4663b122e9da853e7852a43dfa30fdf220cbb6
anthonycalcagno9 <acalcagno9@gmail.com>
2025-07-31 17:32:00 -0600

    changing button

--- my-react-router-app/app/routes/home.tsx
+++ my-react-router-app/app/routes/home.tsx
@@ -48,7 +48,7 @@ export default function Home() {
           }}
           className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
         >
-          Fuck
+          sort meals
         </button>
       </div>
     </div>
